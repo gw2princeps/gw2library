10e61d37f6d31bf0e0b0b1b26969b810151379e3
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-11-18 13:30:09 +0100

    fix: GET build working with nodejs runtime

--- pages/api/builds/get/[buildid].ts
+++ pages/api/builds/get/[buildid].ts
@@ -1,5 +1,5 @@
 import { GetItemCommand } from "@aws-sdk/client-dynamodb";
-import { NextRequest } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { client } from "src/utils/dbclient";
 
 export const config = {
@@ -21,18 +21,21 @@ const BUILDS = {
   },
 };
 
-export default async function getBuild(req: NextRequest) {
+export default async function getBuild(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "GET") {
-    return new Response("Method Not Allowed", { status: 405 });
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
-  if (!req.nextUrl.searchParams.has("buildid")) {
-    return new Response("Missing buildid", { status: 400 });
+  if (!req.query.buildid) {
+    return res.status(400).json({ error: "Missing required fields" });
   }
 
-  const buildid = req.nextUrl.searchParams.get("buildid") || "";
+  const buildid = req.query.buildid as string;
   if (buildid.length === 0) {
-    return new Response("Missing buildid", { status: 400 });
+    return res.status(400).json({ error: "Missing required fields" });
   }
 
   const { Item: build } = await client.send(
@@ -45,21 +48,13 @@ export default async function getBuild(req: NextRequest) {
   );
 
   if (!build) {
-    return new Response("Build not found", { status: 404 });
+    return res.status(404).json({ error: "Build not found" });
   }
-
-  const res = new Response(
-    JSON.stringify({
-      name: build.name.S,
-      mdx: build.mdx.S,
-      chatcode: build.chatcode.S,
-      timestamp: parseInt(build.timestamp.N || "-1", 10),
-      character: JSON.parse(build.character.S || "{}"),
-    }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-  return res;
+  return res.status(200).json({
+    name: build.name.S,
+    mdx: build.mdx.S,
+    chatcode: build.chatcode.S,
+    timestamp: parseInt(build.timestamp.N || "-1", 10),
+    character: JSON.parse(build.character.S || "{}"),
+  });
 }
