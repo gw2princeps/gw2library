7a15cf2ae40d3b92c122a94bca1f7e382f6b1e9e
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-11-24 22:51:27 +0100

    feat: improve table

--- src/components/accountlist/table/Table.tsx
+++ src/components/accountlist/table/Table.tsx
@@ -1,5 +1,6 @@
 import { Button, Classes, HTMLTable, NonIdealState } from "@blueprintjs/core";
-import { Profession } from "@discretize/gw2-ui-new";
+import { Profession, Skill, Specialization } from "@discretize/gw2-ui-new";
+import { NoSelection } from "@discretize/react-discretize-components";
 import Link from "next/link";
 import React from "react";
 import { Build } from "src/types/Build";
@@ -14,8 +15,6 @@ export type TabledBuild = Build & { id: string };
 const Table = () => {
   const { data, error } = useSWR<TabledBuild[]>("/api/account/list", fetcher);
   const loading = !data;
-
-  console.log(`State: ${loading} ${error} ${data}`);
 
   if (error) {
     return <NonIdealState title="Error" description="An error occured" />;
@@ -45,11 +44,18 @@ const Table = () => {
     );
   }
 
+  function TabledSkill({ id }: { id: number | undefined }) {
+    if (!id) return <NoSelection className={classes.bigIcon} />;
+    return <Skill id={id} className={classes.bigIcon} disableText />;
+  }
+
   return (
     <HTMLTable className={classes.table} striped>
       <thead>
         <tr>
           <th>Name</th>
+          <th>Specializations</th>
+          <th>Skills</th>
           <th>Created</th>
 
           <th></th>
@@ -59,12 +65,31 @@ const Table = () => {
         {data?.map((build) => (
           <tr key={build.id}>
             <td>
-              <Profession
-                disableText
-                className={classes.bigIcon}
-                name={build.character.attributes.specialization}
-              />{" "}
-              {build.name}
+              <Link href={`/builds/edit/${build.id}`}>
+                <Profession
+                  disableText
+                  className={classes.bigIcon}
+                  name={build.character.attributes.specialization}
+                />{" "}
+                {build.name}
+              </Link>
+            </td>
+            <td>
+              {build.character.traits.lines.map((lineId) => (
+                <Specialization
+                  key={lineId}
+                  id={lineId}
+                  disableText
+                  className={classes.bigIcon}
+                />
+              ))}
+            </td>
+            <td>
+              <TabledSkill id={build.character.skills?.healId} />
+              <TabledSkill id={build.character.skills?.utility1Id} />
+              <TabledSkill id={build.character.skills?.utility2Id} />
+              <TabledSkill id={build.character.skills?.utility3Id} />
+              <TabledSkill id={build.character.skills?.eliteId} />
             </td>
             <td>{new Date(build.timestamp).toLocaleString()}</td>
             <td>
