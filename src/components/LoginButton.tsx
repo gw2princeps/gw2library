97ea2fd0eb971d5f82789292d7880de792e5ebfa
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-11-27 21:40:43 +0100

    feat: add loading to login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,16 +1,20 @@
 import { Button, Classes } from "@blueprintjs/core";
 import { signIn, useSession } from "next-auth/react";
+import React from "react";
 
 export default function Component(props: Record<string, any>): JSX.Element {
   const { data } = useSession();
+  const [loading, setLoading] = React.useState(false);
 
   if (data) return <></>;
 
   return (
     <>
       <Button
+        loading={loading}
         className={Classes.MINIMAL}
         onClick={() => {
+          setLoading(true);
           if (process.env.NODE_ENV === "production") {
             return signIn("gw2auth");
           } else {
